9f6e67ed271acf1acebdf4fe6b8179bbd59118cb
Justin Durocher <justduro@gmail.com>
2020-03-31 13:49:52 -0400

    models

---
+++ models/Workout.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+    day : {
+        type : Date,
+        default : Date.now
+    },
+
+    totalDuration : {
+        type : Number,
+        default : 0
+    },
+
+    exercises : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Exercise"
+        }
+    ]
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
